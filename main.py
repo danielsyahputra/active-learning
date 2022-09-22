cfcfea108a7bea87da3a8ba3fffd00e3c768213f
danielsyahputra13 <dnlshp@gmail.com>
2022-09-22 10:46:52 +0700

    fix main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from utils.download import download_data
-from utils.torch import dataloader
 
 def main() -> None:
-    dataloaders = dataloader(train_batch_size=64, test_batch_size=16)
-    print(dataloaders)
+    download_data()
 
 if __name__=="__main__":
     main()
